de6831a0f2a7e1f03d6c57a3e24f2125cd42058d
george kettle <georgekettle.gk@gmail.com>
2023-10-10 14:33:48 -0600

    Added shadcn defaults

--- app/views/layouts/application_layout.rb
+++ app/views/layouts/application_layout.rb
@@ -12,6 +12,7 @@ class ApplicationLayout < ApplicationView
 				meta name: "viewport", content: "width=device-width,initial-scale=1"
 				csp_meta_tag
 				csrf_meta_tags
+				stylesheet_link_tag "https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,301,701,300,501,401,901,400&display=swap", data_turbo_track: "reload"
 				stylesheet_link_tag "application", data_turbo_track: "reload"
 				javascript_include_tag "application", data_turbo_track: "reload", defer: true
 			end
