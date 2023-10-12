1809f0736d61a6e720c32e7fdd85f1095f892ba0
george kettle <georgekettle.gk@gmail.com>
2023-10-12 11:39:53 -0600

    Added default type='button' to button component

--- app/views/components/button.rb
+++ app/views/components/button.rb
@@ -1,25 +1,28 @@
 # frozen_string_literal: true
 
 class Button < ApplicationComponent
-    def initialize(variant: :primary, size: nil)
+    def initialize(variant: :primary, size: nil, disabled: false, **attributes)
         @variant = variant
         @size = size
+        @disabled = disabled
+        @attributes = attributes
+        @attributes[:type] ||= "button"
     end
 
     def template(&)
         case @variant
         when :primary
-            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), &)
+            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), disabled: @disabled, **@attributes, &)
         when :link
-            button(class: "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-9 px-4 py-2", &)
+            button(class: "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-9 px-4 py-2", disabled: @disabled, **@attributes, &)
         when :secondary
-            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), &)
+            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), disabled: @disabled, **@attributes, &)
         when :destructive
-            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), &)
+            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), disabled: @disabled, **@attributes, &)
         when :outline
-            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), &)
+            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), disabled: @disabled, **@attributes, &)
         when :ghost
-            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), &)
+            button(class: tokens("inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9", -> { @size == "icon" } => "h-9 w-9", -> { @size.nil? } => "px-4 py-2"), disabled: @disabled, **@attributes, &)
         end
     end
 end
