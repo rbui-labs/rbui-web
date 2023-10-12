9b18f3e5502b66c2a43af148b77bbb6789637774
george kettle <georgekettle.gk@gmail.com>
2023-10-12 10:56:50 -0600

    Added destructive variant of Alert

--- app/views/docs/alert_view.rb
+++ app/views/docs/alert_view.rb
@@ -8,9 +8,17 @@ class Docs::AlertView < ApplicationView
         render Typography::P.new { "Displays a callout for user attention." }
       end
 
-      render Docs::Component.new do
+      render Docs::Component.new(title: "Default") do
         render Alert.new do
-          rocket_icon
+          icon
+          render AlertTitle.new { "Alert Title" }
+          render AlertDescription.new { "Alert description" }
+        end
+      end
+
+      render Docs::Component.new(title: "Destructive") do
+        render Alert.new(variant: :destructive) do
+          icon
           render AlertTitle.new { "Alert Title" }
           render AlertDescription.new { "Alert description" }
         end
@@ -18,21 +26,21 @@ class Docs::AlertView < ApplicationView
     end
   end
 
-  def rocket_icon
+  def icon
     svg(
-        xmlns: "http://www.w3.org/2000/svg",
-        fill: "none",
-        viewbox: "0 0 24 24",
-        stroke_width: "1.5",
-        stroke: "currentColor",
-        class: "w-4 h-4"
-      ) do |s|
-        s.path(
-          stroke_linecap: "round",
-          stroke_linejoin: "round",
-          d:
-            "M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-        )
-      end
+      xmlns: "http://www.w3.org/2000/svg",
+      fill: "none",
+      viewbox: "0 0 24 24",
+      stroke_width: "1.5",
+      stroke: "currentColor",
+      class: "w-4 h-4"
+    ) do |s|
+      s.path(
+        stroke_linecap: "round",
+        stroke_linejoin: "round",
+        d:
+          "M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
+      )
+    end
   end
 end
