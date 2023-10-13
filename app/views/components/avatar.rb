96cd6dabb7337755021eab7d3928817ce042b516
george kettle <georgekettle.gk@gmail.com>
2023-10-13 10:10:16 -0600

    Added Avatar size property

--- app/views/components/avatar.rb
+++ app/views/components/avatar.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 class Avatar < ApplicationComponent
-  def initialize(**attributes)
+  SIZES = {
+    xs: "h-4 w-4 text-[0.5rem]",
+    sm: "h-6 w-6 text-xs",
+    md: "h-10 w-10 text-base",
+    lg: "h-14 w-14 text-xl",
+    xl: "h-20 w-20 text-3xl"
+  }
+
+  def initialize(size: :md, **attributes)
+    @size_classes = SIZES[size]
     @attributes = attributes
   end
 
   def template(&)
     span(
       **@attributes,
-      class: tokens("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", -> { @attributes[:class] } => @attributes[:class]),
+      class: tokens("relative flex #{@size_classes} shrink-0 overflow-hidden rounded-full", -> { @attributes[:class] } => @attributes[:class]),
       &)
   end
 end
--- app/views/docs/avatar_view.rb
+++ app/views/docs/avatar_view.rb
@@ -8,18 +8,45 @@ class Docs::AvatarView < ApplicationView
         render Typography::P.new { "An image element with a fallback for representing the user." }
       end
 
-      render Docs::Component.new(title: 'Image & Fallback') do
+      render Docs::Component.new(title: 'Image & fallback') do
         render Avatar.new do
           render AvatarImage.new(src: "https://avatars.githubusercontent.com/u/246692?v=4", alt: "joeldrapper")
           render AvatarFallback.new { "JD" }
         end
       end
 
-      render Docs::Component.new(title: 'Only Fallback') do
+      render Docs::Component.new(title: 'Only fallback') do
         render Avatar.new do
           render AvatarFallback.new { "JD" }
         end
       end
+
+      render Docs::Component.new(title: 'Sizes') do
+        div(class: 'flex items-center space-x-2') do
+          default_avatar(size: :xs)
+          default_avatar(size: :sm)
+          default_avatar(size: :md)
+          default_avatar(size: :lg)
+          default_avatar(size: :xl)
+        end
+      end
+
+      render Docs::Component.new(title: 'Sizes (only fallback)') do
+        div(class: 'flex items-center space-x-2') do
+          default_avatar(size: :xs, image: false)
+          default_avatar(size: :sm, image: false)
+          default_avatar(size: :md, image: false)
+          default_avatar(size: :lg, image: false)
+          default_avatar(size: :xl, image: false)
+        end
+      end
+    end
+  end
+
+  def default_avatar(size: :md, image: true)
+    render Avatar.new(size: size) do
+      render AvatarImage.new(src: "https://avatars.githubusercontent.com/u/246692?v=4", alt: "joeldrapper") if image
+      render AvatarFallback.new { "JD" }
     end
   end
 end
