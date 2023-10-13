819be585ec859e3a4b63bb1c0d790f6eefb08554
george kettle <georgekettle.gk@gmail.com>
2023-10-13 14:07:53 -0600

    Added full width example to tabs

--- app/views/docs/tabs_view.rb
+++ app/views/docs/tabs_view.rb
@@ -22,6 +22,21 @@ class Docs::TabsView < ApplicationView
           end
         end
       end
+
+      render Docs::VisualCodeExample.new(title: "Full width") do
+        render Tabs.new(default_value: "account", class: 'w-96') do
+          render TabsList.new(class: 'w-full grid grid-cols-2') do
+            render TabsTrigger.new(value: "account") { "Account" }
+            render TabsTrigger.new(value: "profile") { "Profile" }
+          end
+          render TabsContent.new(value: "account") do
+            div(class: "rounded-lg border p-4 bg-background text-foreground") { "Account" }
+          end
+          render TabsContent.new(value: "profile") do
+            div(class: "rounded-lg border p-4 bg-background text-foreground") { "Profile" }
+          end
+        end
+      end
     end
   end
 end
