5171f99a80da89e3e5cfa35fbb8e5ccc88273dec
george kettle <georgekettle.gk@gmail.com>
2023-10-16 14:55:59 -0600

    Custom width for Sheet component

--- app/views/components/sheet_content.rb
+++ app/views/components/sheet_content.rb
@@ -60,11 +60,11 @@ class SheetContent < ApplicationComponent
         when :top
             tokens("fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top", @attributes[:class])
         when :right
-            tokens("fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm", @attributes[:class])
+            tokens("fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-y-0 right-0 h-full border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right", @attributes[:class])
         when :bottom
             tokens("fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom", @attributes[:class])
         when :left
-            tokens("fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm", @attributes[:class])
+            tokens("fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-y-0 left-0 h-full border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left", @attributes[:class])
         end
     end
 end
--- app/views/docs/sheet_view.rb
+++ app/views/docs/sheet_view.rb
@@ -13,28 +13,8 @@ class Docs::SheetView < ApplicationView
           render SheetTrigger.new do
             render Button.new(variant: :outline) { "Open Sheet" }
           end
-          render SheetContent.new do
-            render SheetHeader.new do
-              render SheetTitle.new { "Edit profile" }
-              render SheetDescription.new { "Make changes to your profile here. Click save when you're done." }
-            end
-            render SheetMiddle.new do
-              render AspectRatio.new(aspect_ratio: "16/9") do
-                img(
-                  alt: "Placeholder",
-                  loading: "lazy",
-                  class: "rounded-md object-cover",
-                  style:
-                    "position:absolute;height:100%;width:100%;inset:0;color:transparent",
-                  src:
-                    "https://images.unsplash.com/photo-1667373509687-4c4574541218?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3132&q=80"
-                )
-              end
-            end
-            render SheetFooter.new do
-              render Button.new(variant: :outline, data: { action: 'click->dismissable#dismiss' }) { "Cancel" }
-              render Button.new { "Save" }
-            end
+          render SheetContent.new(class: 'sm:max-w-sm') do
+            profile_form
           end
         end
       end
@@ -48,33 +28,48 @@ class Docs::SheetView < ApplicationView
               render SheetTrigger.new do
                 render Button.new(variant: :outline, class: 'w-full') { side }
               end
-              render SheetContent.new(side: side) do
-                render SheetHeader.new do
-                  render SheetTitle.new { "Edit profile" }
-                  render SheetDescription.new { "Make changes to your profile here. Click save when you're done." }
-                end
-                render Form.new do
-                  render SheetMiddle.new do
-                    render FormSpacer.new do
-                      render FormItem.new do
-                        render Label.new { "Name" }
-                        render Input.new(placeholder: "Joel Drapper") { "Joel Drapper" }
-                      end
-                      render FormItem.new do
-                        render Label.new { "Email" }
-                        render Input.new(placeholder: "joeldrapper@mail.com")
-                      end
-                    end
-                  end
-                  render SheetFooter.new do
-                    render Button.new(variant: :outline, data: { action: 'click->dismissable#dismiss' }) { "Cancel" }
-                    render Button.new(type: "submit") { "Save" }
-                  end
-                end
+              render SheetContent.new(side: side, class: tokens(-> { [:left, :right].include?(side) } => "sm:max-w-sm")) do
+                profile_form
               end
             end
           end
         end
+      end
+
+      render Docs::VisualCodeExample.new(title: "Custom size") do
+        render Sheet.new do
+          render SheetTrigger.new do
+            render Button.new(variant: :outline) { "Open Sheet" }
+          end
+          render SheetContent.new(class: 'w-3/4') do
+            profile_form
+          end
+        end
+      end
+    end
+  end
+
+  def profile_form
+    render SheetHeader.new do
+      render SheetTitle.new { "Edit profile" }
+      render SheetDescription.new { "Make changes to your profile here. Click save when you're done." }
+    end
+    render Form.new do
+      render SheetMiddle.new do
+        render FormSpacer.new do
+          render FormItem.new do
+            render Label.new { "Name" }
+            render Input.new(placeholder: "Joel Drapper") { "Joel Drapper" }
+          end
+          render FormItem.new do
+            render Label.new { "Email" }
+            render Input.new(placeholder: "joeldrapper@mail.com")
+          end
+        end
+      end
+      render SheetFooter.new do
+        render Button.new(variant: :outline, data: { action: 'click->dismissable#dismiss' }) { "Cancel" }
+        render Button.new(type: "submit") { "Save" }
       end
     end
   end
