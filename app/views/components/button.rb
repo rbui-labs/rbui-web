5a33bdc9cc6d6a51dc0fa2759a2a0fc43317e45d
george kettle <georgekettle.gk@gmail.com>
2023-10-17 17:05:57 -0600

    Restyled Link component

--- app/views/components/button.rb
+++ app/views/components/button.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Button < ApplicationComponent
-    def initialize(variant: :primary, size: nil, **attributes)
-        @variant = variant
-        @size = size
+    def initialize(**attributes)
+        @variant = attributes[:variant] || :primary
+        @size = attributes[:size]
         @attributes = attributes
         @attributes[:type] ||= "button"
     end
@@ -14,8 +14,8 @@ class Button < ApplicationComponent
             button(
                 **@attributes, 
                 class: tokens(
-                    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9",
-                    -> { @size == :icon } => "h-9 w-9",
+                    "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9",
+                    -> { @size == :icon } => "h-9 w-9 justify-center",
                     -> { @size.nil? } => "px-4 py-2",
                     -> { @attributes[:class] } => @attributes[:class]
                 ), 
@@ -24,7 +24,7 @@ class Button < ApplicationComponent
             button(
                 **@attributes, 
                 class: tokens(
-                    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-9 px-4 py-2",
+                    "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-9 px-4 py-2",
                     -> { @attributes[:class] } => @attributes[:class]
                 ),
                 &)
@@ -32,8 +32,8 @@ class Button < ApplicationComponent
             button(
                 **@attributes,
                 class: tokens(
-                    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9",
-                    -> { @size == :icon } => "h-9 w-9",
+                    "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9",
+                    -> { @size == :icon } => "h-9 w-9 justify-center",
                     -> { @size.nil? } => "px-4 py-2",
                     -> { @attributes[:class] } => @attributes[:class]
                 ),
@@ -42,8 +42,8 @@ class Button < ApplicationComponent
             button(
                 **@attributes,
                 class: tokens(
-                    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9",
-                    -> { @size == :icon } => "h-9 w-9",
+                    "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9",
+                    -> { @size == :icon } => "h-9 w-9 justify-center",
                     -> { @size.nil? } => "px-4 py-2",
                     -> { @attributes[:class] } => @attributes[:class]
                 ),
@@ -52,8 +52,8 @@ class Button < ApplicationComponent
             button(
                 **@attributes,
                 class: tokens(
-                    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9",
-                    -> { @size == :icon } => "h-9 w-9",
+                    "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9",
+                    -> { @size == :icon } => "h-9 w-9 justify-center",
                     -> { @size.nil? } => "px-4 py-2",
                     -> { @attributes[:class] } => @attributes[:class]
                 ),
@@ -62,8 +62,8 @@ class Button < ApplicationComponent
             button(
                 **@attributes,
                 class: tokens(
-                    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9",
-                    -> { @size == :icon } => "h-9 w-9",
+                    "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9",
+                    -> { @size == :icon } => "h-9 w-9 justify-center",
                     -> { @size.nil? } => "px-4 py-2",
                     -> { @attributes[:class] } => @attributes[:class]
                 ),
