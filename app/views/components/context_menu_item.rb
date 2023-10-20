a77b33713e2e7269812ac11b4c05e719314db9e0
george kettle <georgekettle.gk@gmail.com>
2023-10-20 11:35:47 -0600

    Added ability to move up and down menu items with keyboard

--- app/views/components/context_menu_item.rb
+++ app/views/components/context_menu_item.rb
@@ -15,10 +15,11 @@ class ContextMenuItem < ApplicationComponent
             href: @href,
             role: "menuitem",
             class:
-                "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 pl-8",
+                "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 pl-8",
             tabindex: "-1",
             data_orientation: "vertical",
             data_action: "click->popover#close",
+            data_popover_target: "menuItem",
             data_disabled: @disabled,
             disabled: @disabled
         ) do
--- app/views/components/dropdown_menu_item.rb
+++ app/views/components/dropdown_menu_item.rb
@@ -11,8 +11,9 @@ class DropdownMenuItem < ApplicationComponent
             **@attributes,
             href: @href,
             role: "menuitem",
-            class: tokens("relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50", @attributes[:class]),
+            class: tokens("relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50", @attributes[:class]),
             data_action: "click->popover#close",
+            data_popover_target: "menuItem",
             tabindex: "-1",
             data_orientation: "vertical",
             &
