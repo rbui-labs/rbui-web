558679ec89328bced267598e870bb75fc20c3abc
george kettle <georgekettle.gk@gmail.com>
2023-11-24 12:56:33 +1100

    Updated user migration to include auth_token and auth_token_expires_at

--- db/migrate/20231123053447_create_users.rb
+++ db/migrate/20231123053447_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email
+      t.string :auth_token
+      t.datetime :auth_token_expires_at
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_053447) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "auth_token"
+    t.datetime "auth_token_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
