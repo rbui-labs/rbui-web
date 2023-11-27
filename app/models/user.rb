e911f48a5cd52bbf2fa539240334a26a25561974
george kettle <georgekettle.gk@gmail.com>
2023-11-27 15:09:23 +1100

    Added plan enum field to users [free|personal|team]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    enum plan: { free: 0, personal: 1, team: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_053447) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_27_040224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_053447) do
     t.datetime "auth_token_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plan", default: 0
   end
 
 end
