9ca6205ee01c9e18cdb557b7470d362f0d3ecd96
george kettle <georgekettle.gk@gmail.com>
2023-11-29 14:49:38 +1100

    Updated subscribed method of user to account for whether they are a team member or not

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     enum plan: { free: 0, personal: 1, team: 2 }
 
     def subscribed?
-        personal? || team?
+        personal? || team? || is_team_member?
     end
 
     def not_subscribed?
@@ -18,5 +18,9 @@ class User < ApplicationRecord
 
     def team_members_limit
         errors.add(:team_members, "limit is 25") if team_members.size > 25
+    end
+
+    def is_team_member?
+        TeamMember.exists?(email: email)
     end
 end
