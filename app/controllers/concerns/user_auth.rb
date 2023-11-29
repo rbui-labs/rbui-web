c3b9638060673f10dfc084a50526a132ecff41e2
george kettle <georgekettle.gk@gmail.com>
2023-11-29 15:05:42 +1100

    Updated authorize_user! controller concern to redirect only if there is no user

--- app/controllers/concerns/user_auth.rb
+++ app/controllers/concerns/user_auth.rb
@@ -7,10 +7,12 @@ module UserAuth
 
     def authenticate_user!
         @current_user = User.find_by(id: session[:user_id])
-        raise "No user is present" unless @current_user.present?
-
-        redirect_to new_login_path(
-        redirect_path: request.original_fullpath
-        )
+        
+        if @current_user.nil?
+            flash[:notice] = "You must be signed in."
+            redirect_to new_signin_path(
+                redirect_path: request.original_fullpath
+            )
+        end
     end
 end
