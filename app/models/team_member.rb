c28378ad99b5a1ad434c52f07b165e78724e6c9f
george kettle <georgekettle.gk@gmail.com>
2023-11-29 14:46:42 +1100

    Validations added so that only 25 team members can be added

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,3 +1,9 @@
 class TeamMember < ApplicationRecord
   belongs_to :user
+
+  validates :email, uniqueness: {
+      scope: :user_id,
+      message: "is already a member of your account"
+    },
+    format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    has_many :team_members, dependent: :destroy
+    
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validate :team_members_limit, if: :team?
+    
     enum plan: { free: 0, personal: 1, team: 2 }
 
     def subscribed?
@@ -8,5 +12,11 @@ class User < ApplicationRecord
 
     def not_subscribed?
         !subscribed?
+    end
+
+    private
+
+    def team_members_limit
+        errors.add(:team_members, "limit is 25") if team_members.size > 25
     end
 end
