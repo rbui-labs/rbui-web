4de3be161f1120765f5c819fb1c58804f315a506
george kettle <georgekettle.gk@gmail.com>
2023-11-30 14:19:29 +1100

    Refactored sync_checkout_session_with_user with error catches and logs

--- lib/payment/sync_checkout_session_with_user.rb
+++ lib/payment/sync_checkout_session_with_user.rb
@@ -4,31 +4,62 @@ module Payment
             ENV['STRIPE_PERSONAL_PRODUCT_ID'] => 'personal',
             ENV['STRIPE_TEAM_PRODUCT_ID'] => 'team'
         }
-        
+
         def initialize(id)
             @id = id
         end
 
         def call
-            checkout_session = Stripe::Checkout::Session.retrieve({
+            checkout_session = retrieve_checkout_session
+            return if invalid_checkout_session?(checkout_session)
+
+            user = find_or_create_user(checkout_session.customer_details&.email)
+            update_user_plan(user, checkout_session.line_items)
+            puts "Successfully synced checkout session #{@id} with user #{user.email}"
+        rescue Stripe::StripeError => e
+            log_error("Stripe error: #{e.message}")
+        rescue StandardError => e
+            log_error("Unexpected error: #{e.message}")
+        end
+
+        private
+
+        def retrieve_checkout_session
+            Stripe::Checkout::Session.retrieve({
                 id: @id,
                 expand: ['line_items']
             })
-            email = checkout_session.customer_details.email
-            user = EmailAuth::FindsOrCreatesUser.new(email: email).call
-            
-            # Record which plan the user has purchased
-            lines = checkout_session.line_items
-            lines.each do |line|
-                plan_type = PLAN_TYPES[line.price.product]
+        end
+
+        def invalid_checkout_session?(checkout_session)
+            if not_paid?(checkout_session)
+                puts "Skipping checkout session #{@id} because it's not paid"
+                return true
+            elsif checkout_session.customer_details&.email.nil?
+                puts "Skipping checkout session #{@id} because it has no email"
+                return true
+            end
+            false
+        end
+
+        def not_paid?(checkout_session)
+            checkout_session.payment_status != 'paid'
+        end
+
+        def find_or_create_user(email)
+            EmailAuth::FindsOrCreatesUser.new(email: email).call
+        end
+
+        def update_user_plan(user, purchased_items)
+            purchased_items.each do |item|
+                plan_type = PLAN_TYPES[item.price.product]
                 user.update(plan: plan_type) if plan_type
             end
-        rescue Stripe::StripeError => e
-            # Handle Stripe API errors
-            puts "Stripe error: #{e.message}"
-        rescue StandardError => e
-            # Handle other unexpected errors
-            puts "Unexpected error: #{e.message}"
+        end
+
+        def log_error(message)
+            # Replace this with your preferred error logging method
+            puts message
         end
     end
 end
