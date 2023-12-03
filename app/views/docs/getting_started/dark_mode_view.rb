4d3d665aabc950c9c6a3e1d40bbf5f969d16f011
george kettle <georgekettle.gk@gmail.com>
2023-12-03 18:55:31 +1100

    Updated theme toggle example to be an outline button (Make it more obvious)

--- app/views/docs/getting_started/dark_mode_view.rb
+++ app/views/docs/getting_started/dark_mode_view.rb
@@ -45,44 +45,46 @@ class Docs::GettingStarted::DarkModeView < ApplicationView
           end
         end
 
-        render Docs::VisualCodeExample.new(title: "Toggle component", context: self) do
-          <<~RUBY
-            render PhlexUI::ThemeToggle.new do |toggle|
-              toggle.light_mode do
-                render PhlexUI::Button.new(variant: :ghost, icon: true) do
-                  svg(
-                    xmlns: "http://www.w3.org/2000/svg",
-                    viewbox: "0 0 24 24",
-                    fill: "currentColor",
-                    class: "w-4 h-4"
-                  ) do |s|
-                    s.path(
-                      d:
-                        "M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
-                    )
+        div(class: 'pt-4') do
+          render Docs::VisualCodeExample.new(title: "Toggle component", context: self) do
+            <<~RUBY
+              render PhlexUI::ThemeToggle.new do |toggle|
+                toggle.light_mode do
+                  render PhlexUI::Button.new(variant: :outline, icon: true) do
+                    svg(
+                      xmlns: "http://www.w3.org/2000/svg",
+                      viewbox: "0 0 24 24",
+                      fill: "currentColor",
+                      class: "w-4 h-4"
+                    ) do |s|
+                      s.path(
+                        d:
+                          "M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
+                      )
+                    end
                   end
                 end
-              end
 
-              toggle.dark_mode do
-                render PhlexUI::Button.new(variant: :ghost, icon: true) do
-                  svg(
-                    xmlns: "http://www.w3.org/2000/svg",
-                    viewbox: "0 0 24 24",
-                    fill: "currentColor",
-                    class: "w-4 h-4"
-                  ) do |s|
-                    s.path(
-                      fill_rule: "evenodd",
-                      d:
-                        "M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z",
-                      clip_rule: "evenodd"
-                    )
+                toggle.dark_mode do
+                  render PhlexUI::Button.new(variant: :outline, icon: true) do
+                    svg(
+                      xmlns: "http://www.w3.org/2000/svg",
+                      viewbox: "0 0 24 24",
+                      fill: "currentColor",
+                      class: "w-4 h-4"
+                    ) do |s|
+                      s.path(
+                        fill_rule: "evenodd",
+                        d:
+                          "M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z",
+                        clip_rule: "evenodd"
+                      )
+                    end
                   end
                 end
               end
-            end
-          RUBY
+            RUBY
+          end
         end
       end
     end
