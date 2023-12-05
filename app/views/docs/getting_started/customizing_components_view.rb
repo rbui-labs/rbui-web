5370a006da5ebd278f807d1f6a481f52f02e7b94
george kettle <georgekettle.gk@gmail.com>
2023-12-06 10:18:47 +1100

    Removed attrs section on customizing components

--- app/views/docs/getting_started/customizing_components_view.rb
+++ app/views/docs/getting_started/customizing_components_view.rb
@@ -15,6 +15,19 @@ class Docs::GettingStarted::CustomizingComponentsView < ApplicationView
         render PhlexUI::Typography::P.new do
           plain "All components accept any HTML attribute, and will pass it through to the underlying HTML element. This is great for quick changes, or when you need to add a custom class for a one off situation."
         end
+        render PhlexUI::Alert.new do
+          puzzle_icon
+          render PhlexUI::Alert::Title.new { "How it works" }
+          render PhlexUI::Alert::Description.new do
+            plain "When you pass an attribute to a PhlexUI component, it will be processed by the"
+            render PhlexUI::Typography::InlineCode.new { "PhlexUI::AttributeMerger" }
+            plain " class. You can read more about the "
+            render PhlexUI::Typography::InlineLink.new(href: "https://github.com/PhlexUI/phlex_ui/blob/main/lib/phlex_ui/base.rb") { "PhlexUI::Base" }
+            plain " and the "
+            render PhlexUI::Typography::InlineLink.new(href: "https://github.com/PhlexUI/phlex_ui/blob/main/lib/phlex_ui/attribute_merger.rb") { "PhlexUI::AttributeMerger" }
+            plain " classes on Github."
+          end
+        end
         render PhlexUI::Typography::Large.new { "Adding attributes" }
         render PhlexUI::Typography::P.new do
           plain "By default, attribute values are added to the existing values of the component. For instance, if you want to make a button span the full width of its container, you can do it like this: "
@@ -128,5 +141,25 @@ class Docs::GettingStarted::CustomizingComponentsView < ApplicationView
         end
       end
     RUBY
+  end
+
+  private
+
+  def puzzle_icon
+    svg(
+      xmlns: "http://www.w3.org/2000/svg",
+      fill: "none",
+      viewbox: "0 0 24 24",
+      stroke_width: "1.5",
+      stroke: "currentColor",
+      class: "w-5 h-5"
+    ) do |s|
+      s.path(
+        stroke_linecap: "round",
+        stroke_linejoin: "round",
+        d:
+          "M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z"
+      )
+    end
   end
 end
