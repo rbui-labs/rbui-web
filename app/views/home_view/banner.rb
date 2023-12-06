bfa19b6da715ee56624f0ce7cf3fa676bd49d83b
george kettle <georgekettle.gk@gmail.com>
2023-12-06 21:42:05 +1100

    Fixed home banner alignment

--- app/views/home_view/banner.rb
+++ app/views/home_view/banner.rb
@@ -7,8 +7,8 @@ class HomeView::Banner < ApplicationComponent
         div(class: 'relative overflow-hidden') do
             render Shared::GridPattern.new(spacing: :md)
             render HomeView::Shapes.new(size: :xl, color: :pink, class: 'hidden sm:block absolute right-0 top-1/2 transform -translate-y-1/2 sm:translate-x-2/3 md:translate-x-1/2')
-            div(class: "relative container mx-auto max-w-3xl py-24 lg:py-32 px-4 text-center flex flex-col items-center gap-y-6") do
-                render PhlexUI::Typography::H1.new(class: 'text-center') do
+            div(class: "relative container mx-auto max-w-3xl py-24 lg:py-32 px-4 sm:text-center flex flex-col sm:items-center gap-y-6") do
+                render PhlexUI::Typography::H1.new do
                     plain "A UI component library, crafted precisely for Ruby devs"
                     span(class: 'text-foreground opacity-30') { " who want to stay organised and build modern apps, fast." }
                 end
