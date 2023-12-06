b5b6c5d897508697d4614f2ce1302856205145b4
george kettle <georgekettle.gk@gmail.com>
2023-12-06 13:47:59 +1100

    Added components reference to Calendar

--- app/views/docs/calendar_view.rb
+++ app/views/docs/calendar_view.rb
@@ -9,6 +9,8 @@ class Docs::CalendarView < ApplicationView
     div(class: "max-w-2xl mx-auto w-full py-10 space-y-10") do
       render Docs::Header.new(title: "Calendar", description: "A date field component that allows users to enter and edit date.", premium: @premium)
 
+      render PhlexUI::Typography::H2.new { "Usage" }
+
       render Docs::VisualCodeExample.new(title: 'Connect to input', context: self, premium: @premium) do
         <<~RUBY
           div(class: 'space-y-4') do
@@ -27,7 +29,22 @@ class Docs::CalendarView < ApplicationView
         RUBY
       end
 
-      render Docs::InstallationInstructionsComingSoon.new
+      render Docs::ComponentsTable.new(components)
     end
+  end
+
+  private
+
+  def components
+    [
+      Docs::ComponentStruct.new("PhlexUI::Calendar", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Body", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/body.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Days", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/days.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Header", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/header.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Next", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/next.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Prev", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/prev.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Title", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/title.rb"),
+      Docs::ComponentStruct.new("PhlexUI::Calendar::Weekdays", "https://github.com/PhlexUI/phlex_ui_pro/blob/main/lib/phlex_ui_pro/calendar/weekdays.rb"),
+    ]
   end
 end
