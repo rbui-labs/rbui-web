fc427764036c6ffd07be7ccb70429b7445c53d56
george kettle <georgekettle.gk@gmail.com>
2023-12-07 14:22:29 +1100

    User auth sets Current.user every request

--- app/controllers/concerns/user_auth.rb
+++ app/controllers/concerns/user_auth.rb
@@ -2,18 +2,24 @@ module UserAuth
     extend ActiveSupport::Concern
 
     included do
+        before_action :set_current_user
         before_action :authenticate_user!
-        attr_reader :current_user
+    end
+
+    def set_current_user
+        Current.user = User.find_by(id: session[:user_id])
     end
 
     def authenticate_user!
-        @current_user = User.find_by(id: session[:user_id])
-        
-        if @current_user.nil?
+        if Current.user.nil?
             flash[:notice] = "You must be signed in."
             redirect_to new_signin_path(
                 redirect_path: request.original_fullpath
             )
         end
+    end
+
+    def current_user
+        Current.user
     end
 end
