bd4fb82e3a946f334cdfbe3f4e518a4a44d43e07
george kettle <georgekettle.gk@gmail.com>
2023-12-12 11:40:46 +1100

    Added intallation step

--- app/views/payments/confirmation_view.rb
+++ app/views/payments/confirmation_view.rb
@@ -29,6 +29,7 @@ class Payments::ConfirmationView < ApplicationView
           div(class: 'flex flex-col space-y-12 pt-10') do
             sign_in_step
             github_step
+            installation_step
           end
         end
       end
@@ -37,6 +38,18 @@ class Payments::ConfirmationView < ApplicationView
 
   private
 
+  def installation_step
+    check_list_item(title: "Install PhlexUI", description: 'Install PhlexUI in your Ruby app.', checked: false) do
+      render PhlexUI::Card.new(class: 'shadow-none p-6 space-y-4') do
+        render PhlexUI::Typography::P.new { "Follow the installation guide to install PhlexUI in your Ruby app." }
+        render PhlexUI::Link.new(href: helpers.docs_installation_path, variant: :primary, class: 'w-full') do 
+          plain "Go to installation guide"
+          arrow_right_icon
+        end
+      end
+    end
+  end
+
   def github_step
     check_list_item(title: "Connect your Github", description: 'Access the private PhlexUI repositories on GitHub.', checked: Current.user&.github_username.present?) do
       if Current.user&.github_username.present?
@@ -44,7 +57,7 @@ class Payments::ConfirmationView < ApplicationView
           render PhlexUI::Alert::Description.new { "Github connected. You can now access the private github repositories" }
         end
       else
-        render PhlexUI::Card.new(class: 'bg-muted shadow-none p-6') do
+        render PhlexUI::Card.new(class: 'shadow-none p-6') do
           github_form
         end
       end
@@ -58,7 +71,7 @@ class Payments::ConfirmationView < ApplicationView
           render PhlexUI::Alert::Description.new { "Nice work! You're signed in." }
         end
       else
-        render PhlexUI::Card.new(class: 'bg-muted shadow-none p-6') do
+        render PhlexUI::Card.new(class: 'shadow-none p-6') do
           sign_in_form
         end
       end
@@ -178,6 +191,23 @@ class Payments::ConfirmationView < ApplicationView
         stroke_linejoin: "round",
         d:
           "M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+      )
+    end
+  end
+
+  def arrow_right_icon
+    svg(
+      xmlns: "http://www.w3.org/2000/svg",
+      fill: "none",
+      viewbox: "0 0 24 24",
+      stroke_width: "1.5",
+      stroke: "currentColor",
+      class: "w-4 h-4 ml-2 mr-1.5"
+    ) do |s|
+      s.path(
+        stroke_linecap: "round",
+        stroke_linejoin: "round",
+        d: "M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
       )
     end
   end
