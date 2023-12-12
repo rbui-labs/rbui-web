905f95a87199cc40acbc2992228d0a6c2d5a8498
george kettle <georgekettle.gk@gmail.com>
2023-12-12 15:51:53 +1100

    removed marketing mailer

--- app/views/mailers/team_member_mailer/invite.rb
+++ app/views/mailers/team_member_mailer/invite.rb
@@ -1,28 +1,28 @@
 module Mailers
-    module TeamMemberMailer
-        class Invite < ::Mailers::ApplicationMailer
-            def initialize(user:, email:)
-                @user = user
-                @email = email
-            end
-            
-            def template
-                div(class: '!block flex justify-center', style: 'display: none;') do
-                    render Shared::Logo.new
-                end
-                render PhlexUI::Card.new(class: "p-6 flex flex-col items-center gap-y-4 max-w-lg mx-auto") do
-                    render PhlexUI::Typography::Muted.new do
-                        plain "You've been invited by "
-                        strong(class: 'font-semibold !text-foreground') { @user.email } 
-                        plain " to join #{ENV['APP_NAME']}. Click the link below and make sure to sign in with the following email - "
-                        strong(class: 'font-semibold !text-foreground') { @email }
-                    end
-                    render PhlexUI::Link.new(
-                        variant: :primary, 
-                        href: helpers.new_signin_url + "?email=#{CGI.escape(@email)}"
-                    ) { "Sign in to PhlexUI" }
-                end
-            end
+  module TeamMemberMailer
+    class Invite < ::Mailers::ApplicationMailer
+      def initialize(user:, email:)
+        @user = user
+        @email = email
+      end
+      
+      def template
+        div(class: '!block flex justify-center', style: 'display: none;') do
+          render Shared::Logo.new
         end
+        render PhlexUI::Card.new(class: "p-6 flex flex-col items-center gap-y-4 max-w-lg mx-auto") do
+          render PhlexUI::Typography::Muted.new do
+            plain "You've been invited by "
+            strong(class: 'font-semibold !text-foreground') { @user.email } 
+            plain " to join #{ENV['APP_NAME']}. Click the link below and make sure to sign in with the following email - "
+            strong(class: 'font-semibold !text-foreground') { @email }
+          end
+          render PhlexUI::Link.new(
+            variant: :primary, 
+            href: helpers.new_signin_url + "?email=#{CGI.escape(@email)}"
+          ) { "Sign in to PhlexUI" }
+        end
+      end
     end
+  end
 end
