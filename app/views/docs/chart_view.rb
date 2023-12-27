b07f974b670fd905a1f3d76963bd011e45ef1290
george kettle <georgekettle.gk@gmail.com>
2023-12-27 16:15:26 +1100

    Updated text for introduction to Chart component

--- app/views/docs/chart_view.rb
+++ app/views/docs/chart_view.rb
@@ -12,7 +12,7 @@ class Docs::ChartView < ApplicationView
         render PhlexUI::Typography::InlineLink.new(href: "https://www.chartjs.org/") { "Chart.js" }
         plain " to render charts. Chart.js is a free open-source JavaScript library for data visualization, which supports 8 chart types: bar, line, area, pie, bubble, radar, polar, and scatter. If you're unfamiliar with Chart.js. We recommend the "
         render PhlexUI::Typography::InlineLink.new(href: "https://www.chartjs.org/docs/latest/getting-started/") { "Getting Started guide" }
-        plain "."
+        plain ". "
       end
 
       render PhlexUI::Typography::H2.new { "Usage" }
